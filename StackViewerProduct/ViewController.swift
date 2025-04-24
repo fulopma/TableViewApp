53ea6d757cd4f1a95a07c4c9fb2df1b7c91630ee
Marcell Fulop <marcell.fulop@techconsulting.tech>
2025-04-24 13:10:47 -0400

    Initial Commit

---
+++ StackViewerProduct/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackViewerProduct
+//
+//  Created by Marcell Fulop on 4/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
