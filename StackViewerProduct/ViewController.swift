8ed253f3682185948b8a7b61740528866a11705f
Marcell Fulop <marcell.fulop@techconsulting.tech>
2025-04-24 15:28:18 -0400

    100 alternating rgb colors

--- StackViewerProduct/ViewController.swift
+++ StackViewerProduct/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var ColorTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        ColorTable.dataSource = self
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        
+        let colorChoice = indexPath.row % 3
+        switch colorChoice{
+        case 0:
+            cell.backgroundColor = UIColor.red
+            break
+        case 1:
+            cell.backgroundColor = UIColor.green
+            break
+        default:
+            cell.backgroundColor = UIColor.blue
+        }
+        return cell 
+    }
 
 }
 
